7bc3a4fc4acfd318b0bba5d3e97042bfc081c47c
MarceloSLeal <marcelosleal@outlook.com>
2023-10-21 18:53:20 -0300

    First commit

---
+++ primeiro-exemplo/user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
+import com.ms.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserProducer userProducer;
+
+    @Transactional
+    public UserModel save(UserModel userModel) {
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
+    }
+
+}
